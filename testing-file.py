83997c5fecdcef65aec51827d34b4f73e4174fa2
saadkhan23 <42278578+saadkhan23@users.noreply.github.com>
2018-08-11 17:01:37 +0400

    Update testing-file.py

--- testing-file.py
+++ testing-file.py
@@ -5,11 +5,50 @@ print(sys.version)
 # plot using matplotlib pyplot
 import pandas as pd
 import numpy as np
+import holtwinters as hw
+import csv
+import matplotlib
+import matplotlib.pyplot as plt
+#%matplotlib inline
+matplotlib.style.use('ggplot')
+from datetime import datetime as dt
+from math import sqrt
+from operator import add
 from pandas import DataFrame
 
-sales = pd.read_excel("C:\Users\saadk\Desktop\Saadswork\janfull.xlsx", header=None, names=['Date','Sales'])   #read the csv file (put 'r' before the path string to address any special characters, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-df = pd.DataFrame(sales)
-df['Date'] = pd.to_datetime(df['Date'], unit='s')
+with open ("C:\Users\saadk\Desktop\Saadswork\janfull.csv", 'rU') as f:
+    reader = csv.reader(f)
+    col_names = reader.next()
+    rows = [(dt.strptime(row[0], '%y-%b'), float(row[1])) for row in reader]
+sales_df = pd.DataFrame(rows, columns = col_names)
+raw_ls = list(sales_df['sales'])
+
+# optimized parameters
+hw_fc, alpha, beta, rmse = hw.linear(raw_ls,3)
+
+print "alpha:%s, beta:%s" %(alpha, beta)
+print "rmse: %s, frac_rmse: %s" %(rmse, rmse/np.mean(raw_ls))
+
+plt.plot(raw_ls, color = 'red', marker = 'o', markersize = 4)
+plt.plot([None]*(len(raw_ls)-1) + [raw_ls[-1]] + hw_fc, color = 'blue', marker = 'o', alpha=.4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#sales = pd.read_excel("C:\Users\saadk\Desktop\Saadswork\janfull.xlsx", header=None, names=['Date','Sales'])   #read the csv file (put 'r' before the path string to address any special characters, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+#df = pd.DataFrame(sales)
+#df['Date'] = pd.to_datetime(df['Date'], unit='s')
 #df = DataFrame(sales,columns=['Date','Sales'])    # assign column names
 #print (df)
 
@@ -23,5 +62,5 @@ df['Date'] = pd.to_datetime(df['Date'], unit='s')
 #sales.plot()
 #plt.show()
 #print(sales.describe())
-print(sales)
+#print(sales)
 #print(sales.describe())
